f3cb970f157b8a5b999d48625a8498eb88afc0fe
CharizBaco <bacocharizmae@gmail.com>
2023-11-19 21:55:23 +0800

    baco

---
+++ src/User.jsx
@@ -0,0 +1,83 @@
+export const users = [
+    {
+      id: 1,
+      name: {
+        firstname: "MAE FLOR",
+        middlename: "",
+        lastname: "BUTAWAN",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "CHARIZ",
+        middlename: "T",
+        lastname: "BACO",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "ANALIZA",
+        middlename: "",
+        lastname: "BARBANIDA",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "KIM",
+        middlename: "",
+        lastname: "COMEGHOD",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "JOVERT",
+        middlename: "",
+        lastname: "DAVIDRAO",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "MARK",
+        middlename: "C",
+        lastname: "DIEZ",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "JAMES",
+        middlename: "",
+        lastname: "ESTOCADO",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "JEFF",
+        middlename: "",
+        lastname: "ESTOREON",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "RAY",
+        middlename: "",
+        lastname: "ESTOROSAS",
+      },
+      address: "Los Angeles, California",
+    },
+  ];
